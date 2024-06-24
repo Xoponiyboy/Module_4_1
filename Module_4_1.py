9e998c857af4c2f28dffda22e1c953083585e5a2
Xoponiyboy <ilyailyanov@yandex.ru>
2024-06-24 07:05:52 +0300

    Add files via upload

---
+++ Module_4_1.py
@@ -0,0 +1,11 @@
+from Module_4_1_FM import fake_divide
+from Module_4_1_TM import true_divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
